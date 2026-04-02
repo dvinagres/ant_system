28f4c5338683db1ff4031abc4443eca187f2ed10
dvinagres <dvinagre93@gmail.com>
2026-04-02 16:27:13 +0200

    Graph changes

--- graph.py
+++ graph.py
@@ -11,15 +11,18 @@ class Graph:
     · n_ants is the number of ants participating
     """
     def __init__(self, points, edge_list, n_ants):
-        self.point_set = set(points)
+        self.points = points
         self.edge_list = edge_list
-        self.total_ants = n_ants
-        self.n_points = len(self.point_set)
+        self.n_ants = n_ants
+        self.ants = []
+        self.n_points = len(self.points)
         self.template_matrix = np.zeros((self.n_points, self.n_points), dtype=int)
         self.points_dict = {} 
+        self.time = 0
+        self.cycle = 0
         
         value = 0
-        for i in self.point_set:
+        for i in self.points:
             self.points_dict[i] = value
             value += 1
 
@@ -28,6 +31,25 @@ class Graph:
         self.trail_matrix = np.copy(self.template_matrix)
         self.delta_matrix = np.copy(self.template_matrix)
     
+    # ants in different points at t = 0
+    def initialize_ants(self):
+        for ant in self.ants:
+            probs = [1/self.n_points for i in range(self.n_points)]
+            # uniform distibution for selecting starting point
+            ant_start = np.random.choice(self.points, p=probs)
+            ant.starting_point = ant_start
+            p1 = self.points_dict[ant_start]
+            self.n_ants_matrix[p1, p1] += 1
+
+    # initial value c for trail intensity
+    def initialize_trail(self):
+        c = 0.5
+        for edge in self.edge_list:
+            p1 = self.points_dict[edge[0]]
+            p2 = self.points_dict[edge[1]]
+
+            self.trail_matrix[p1, p2] = c
+
     # n_ants_point gives the number of ants on a given point
     def n_ants_point(self, point):
         coord = self.points_dict[point]
