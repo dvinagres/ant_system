40bf772f0365740839ccc0ba1fd05c3595e8d616
dvinagres <dvinagre93@gmail.com>
2026-04-02 19:33:03 +0200

    Finished algorithm

--- algorithm.py
+++ algorithm.py
@@ -7,7 +7,9 @@ from graph import Graph
 from ant import Ant
 from calculate import Calculate
 
-# 1. Graph
+"""
+1. Graph
+"""
 # complete graph
 points = ["a", "b", "c", "d", "e"]
 edge_list = [("a", "b"), ("a", "c"), ("a", "d"), ("a", "e"),
@@ -23,23 +25,50 @@ ants = [ant1, ant2, ant3]
 calc = Calculate(graph, ants)
 dist_list = [1, 5, 3, 6, 3, 4, 5, 8, 9, 2]
 
-"""
-· initialize ants starting point
-· initialize trail matrix 
-· delta matrix already initialize with 0s
-"""
 graph.ants = ants
-graph.initialize_ants()
+# initialize trail matrix 
 graph.initialize_trail()
 graph.initialize_dist(dist_list)
 
-# 2. Place starting point in visited for every ant
-for ant in graph.ants:
-    ant.visited_points.add(ant.starting_point)
+# delta matrix already initialize with 0s
 
-# 3. Choose next_point    
-calc.transition_probability()
+# number of cycles
+max_cycles = 50
 
+"""
+2. Main loop
+"""
+for cycle in range(max_cycles):
 
+    # clean previous tour and previous visited_points
+    for ant in graph.ants:
+        ant.tour = []
+        ant.visited_points = set()
+
+    # initialize ants
+    graph.initialize_ants()
+
+    # place starting point in visited for every ant
+    for ant in graph.ants:
+        ant.visited_points.add(ant.starting_point)
+
+    # choose next_point 
+    for iter in range(graph.n_points - 1):
+        calc.transition_probability()
+
+    # back to starting point
+    for ant in graph.ants:
+        current_point = ant.tour[-1][1]
+        ant.tour.append((current_point, ant.starting_point))
+
+    # update trail
+    tours = []
+    for ant in graph.ants:
+        tours.append(ant.tour)
+    calc.update_intensity(tours)
+
+print(ant1.tour)
+print(ant2.tour)
+print(ant3.tour)
 
 
